04034b18205f25c8a5d87cf2738f66476f565dcc
Brian Wang <bwbrianwang1018@gmail.com>
2020-09-17 21:45:43 -0700

    Swapped background images and member images

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
+from research import app
 
 if __name__ == "__main__":
+    # app.run()
     app.run(debug=True)
